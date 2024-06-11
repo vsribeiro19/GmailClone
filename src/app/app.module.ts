ec47e41ada38877f8eff69bb9d69cc7b925dcc95
Victor Ribeiro <v.leitteribeiro@gmail.com>
2024-06-10 21:19:26 -0300

    build: Npm install

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { EmailBoxComponent } from './components/email-box/email-box.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    EmailBoxComponent
   ],
   imports: [
     BrowserModule,
