ee09b83740f6114f45fa7a55a8af75ae30973843
Victor Ribeiro <v.leitteribeiro@gmail.com>
2024-06-10 21:18:24 -0300

    feat: Criar componentes header, sidebar e email-box

---
+++ src/app/components/email-box/email-box.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-box',
+  templateUrl: './email-box.component.html',
+  styleUrls: ['./email-box.component.scss']
+})
+export class EmailBoxComponent {
+
+}
